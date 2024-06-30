7b6f185d7a1ef6a8c423eb1e341008b7582ca980
Роман Максимов <r.maximov@banki.ru>
2024-06-30 16:46:30 +0300

    test

--- src/components/GenerateHistory.jsx
+++ src/components/GenerateHistory.jsx
@@ -6,6 +6,7 @@ export const GenerateHistory = () => {
 
     return (
         <div>
+            Изменения
             {data.map((text) => (
                 <p key={text}>
                     {text}
