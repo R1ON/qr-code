839eb19e7c2259dd9f0ee8a550f7d9b3d538f45c
Роман Максимов <r.maximov@banki.ru>
2024-07-03 21:48:47 +0300

    fixed or not

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,10 +4,10 @@ import s from './Navigation.module.css';
 export const Navigation = () => {
     return (
         <nav className={s.container}>
-            <Link to="/generate">Генерировать QR код</Link>
-            <Link to="/scan">Сканировать QR код</Link>
-            <Link to="/generateHistory">История генерирования</Link>
-            <Link to="/scanHistory">История сканирования</Link>
+            <Link to="/qr-code/generate">Генерировать QR код</Link>
+            <Link to="/qr-code/scan">Сканировать QR код</Link>
+            <Link to="/qr-code/generateHistory">История генерирования</Link>
+            <Link to="/qr-code/scanHistory">История сканирования</Link>
         </nav>
     );
 };
